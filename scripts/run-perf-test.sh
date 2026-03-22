ef3882a3d6ba5515106119eb473a689bd680206c
tae2089 <tae2089002@gmail.com>
2026-03-22 12:18:42 +0900

    fix(perf): improve testbed stability, explicit container names and gateways

--- scripts/run-perf-test.sh
+++ scripts/run-perf-test.sh
@@ -21,7 +21,7 @@ echo "1. Throughput Test (TCP)"
 docker exec client iperf3 -c 172.30.0.2 -t 10 | tee perf-throughput-tcp.log
 
 echo "2. RPS Test (HTTP)"
-docker exec client wrk -t4 -c100 -d10s http://172.30.0.2/ | tee perf-rps.log
+docker exec client wrk -t4 -c100 -d10s http://172.30.0.2:80/ | tee perf-rps.log
 
 echo "3. Latency Test (Ping)"
 docker exec client ping -c 10 172.30.0.2 | tee perf-latency.log
